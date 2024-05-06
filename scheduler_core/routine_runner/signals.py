cf011516fea23a78592d8c2d1897356c5c55aee2
ArifTF <arif.techforing@gmail.com>
2024-05-06 16:08:49 +0600

    Added clocked scheduled functionalities

--- scheduler_core/routine_runner/signals.py
+++ scheduler_core/routine_runner/signals.py
@@ -7,27 +7,43 @@ from django_celery_beat.models import CrontabSchedule, PeriodicTask, ClockedSche
 
 
 @receiver(post_save, sender=CronJobModel)
-def crontab_connector_signal(sender, instance, created, **kwargs):
+def scheduler_connector_signal(sender, instance, created, **kwargs):
     if created:
-        cron_expr = instance.cron_expression
-        splitted_cron_exrp = cron_expr.split(" ")
-        print(f"spillet_cron_exrp: {splitted_cron_exrp}")
-        crontab = CrontabSchedule.objects.create(
-            minute=splitted_cron_exrp[0],
-            hour=splitted_cron_exrp[1],
-            day_of_month=splitted_cron_exrp[2],
-            month_of_year=splitted_cron_exrp[3],
-            day_of_week=splitted_cron_exrp[4],
-            timezone=instance.timezone
-        )
-        instance.cronsched = crontab
-        instance.save()
+        if instance.cron_expression:
+            cron_expr = instance.cron_expression
+            splitted_cron_exrp = cron_expr.split(" ")
+            print(f"splitted_cron_exrp: {splitted_cron_exrp}")
+            crontab = CrontabSchedule.objects.create(
+                minute=splitted_cron_exrp[0],
+                hour=splitted_cron_exrp[1],
+                day_of_month=splitted_cron_exrp[2],
+                month_of_year=splitted_cron_exrp[3],
+                day_of_week=splitted_cron_exrp[4],
+                timezone=instance.cron_timezone
+            )
+            instance.cronsched = crontab
+            instance.save()
 
-        PeriodicTask.objects.create(
-            crontab=crontab,
-            name=instance.title,
-            task="routine_runner.tasks.save_joke_task",
-            args=json.dumps([crontab.pk]),
-        )
+            PeriodicTask.objects.create(
+                crontab=crontab,
+                name=instance.title,
+                task="routine_runner.tasks.save_joke_crontab_task",
+                args=json.dumps([crontab.pk]),
+            )
+        elif instance.scheduled_datetime:
+            print(f"scheduled_datetime: {instance.scheduled_datetime}")
+            clocked = ClockedSchedule.objects.create(
+                clocked_time=instance.scheduled_datetime
+            )
+            instance.clockedsched = clocked
+            instance.save()
+
+            PeriodicTask.objects.create(
+                clocked=clocked,
+                name=instance.title,
+                one_off=True,
+                task="routine_runner.tasks.save_joke_clocked_task",
+                args=json.dumps([clocked.pk]),
+            )
 
     
