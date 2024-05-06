16fdb89d232af85029a65f8d711e08c09a71d8d5
ArifTF <arif.techforing@gmail.com>
2024-05-06 15:45:31 +0600

    Added crontab functionalities

--- scheduler_core/routine_runner/apps.py
+++ scheduler_core/routine_runner/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save
 
 class RoutineRunnerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'routine_runner'
+
+    def ready(self) -> None:
+        from routine_runner.signals import crontab_connector_signal
+        from routine_runner.models import CronJobModel
+        post_save.connect(crontab_connector_signal, sender=CronJobModel)
